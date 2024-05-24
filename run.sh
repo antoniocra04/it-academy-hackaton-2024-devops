a82fd131fbacdd2fee3e9b05ca78ab91d809ecfb
inittose <pol.konnik@gmail.com>
2024-05-24 18:55:57 +0700

    feature(docker-compose): Попытка объединить back и front

---
+++ run.sh
@@ -0,0 +1,10 @@
+#bin/bash
+
+git submodule update --remote it-academy-hackaton-2024-front
+git submodule update --remote it-academy-hackaton-2024-back
+
+
+docker-compose up -d
+
+echo Site: http://localhost:8081/
+echo Swagger: http://localhost:8080/swagger/index.html
